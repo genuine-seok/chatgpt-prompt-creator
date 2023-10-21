5b3f591203699b4dc2553ae01eeac910f41e2ea7
genuine-seok <genuineseok@gmail.com>
2023-10-21 15:42:36 +0900

    feat: api 및 스타일 초기 설정 추가
    - 초기 스타일 reset
    - ai chat api 핸들러 추가

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    OPENAI_API_KEY: process.env.OPENAI_API_KEY ?? "",
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
   },
 };
 
---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,24 @@
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import OpenAI from 'openai';
+
+import { NextRequest } from 'next/server';
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY ?? '',
+});
+
+export const runtime = 'edge';
+
+export const POST = async (req: NextRequest) => {
+  const { messages } = await req.json();
+
+  const response = await openai.chat.completions.create({
+    model: 'gpt-3.5-turbo',
+    stream: true,
+    messages,
+  });
+
+  const stream = OpenAIStream(response);
+
+  return new StreamingTextResponse(stream);
+};
